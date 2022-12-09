2b1b64e8bc85544260dc1c5476f782cf38a7b459
bellljun3395 <195850@jnu.ac.kr>
2022-12-09 23:03:50 +0900

    refactor(IptimeAPIApplication): IptimeAPIApplication 원복

--- src/main/java/com/example/iptimeAPI/IptimeAPIApplication.java
+++ src/main/java/com/example/iptimeAPI/IptimeAPIApplication.java
@@ -1,21 +1,13 @@
 package com.example.iptimeAPI;
 
-import com.example.iptimeAPI.util.iptime.Iptime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class IptimeAPIApplication {
-
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(IptimeAPIApplication.class, args);
-		Iptime iptime = new Iptime();
-		String cookieValue = iptime.getCookieValue();
-		iptime.login(cookieValue);
-		iptime.getList(cookieValue);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IptimeAPIApplication.class, args);
+    }
 
 }
