457443f4d230191f3df14cdef98db1f654b8a0c6
bellljun3395 <195850@jnu.ac.kr>
2022-12-10 03:49:16 +0900

    refactor(ClubsController): ClubsController 리턴 변경
    - @Controller -> @RestController

--- src/main/java/com/example/iptimeAPI/controller/clubs/ClubsController.java
+++ src/main/java/com/example/iptimeAPI/controller/clubs/ClubsController.java
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/clubs")
 public class ClubsController {
