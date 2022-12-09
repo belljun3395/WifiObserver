342b608b6aee0d78ed43fb7302f3013216d607ce
bellljun3395 <195850@jnu.ac.kr>
2022-12-09 23:34:53 +0900

    feat(MacAddressRepository): MacAddressRepository 구현

---
+++ src/main/java/com/example/iptimeAPI/domain/macAddress/MacAddressRepository.java
@@ -0,0 +1,11 @@
+package com.example.iptimeAPI.domain.macAddress;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MacAddressRepository extends JpaRepository<MacAddress, Long> {
+
+    Optional<MacAddress> findByMemberId(Long memberId);
+
+}
