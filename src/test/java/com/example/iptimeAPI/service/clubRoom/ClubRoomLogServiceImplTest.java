4f015ab93986ae5c0cc838851dd6e012b62a4504
bellljun3395 <195850@jnu.ac.kr>
2022-12-10 03:34:19 +0900

    test(ClubRoomLogServiceImplTest): ClubRoomLogServiceImplTest 구현

---
+++ src/test/java/com/example/iptimeAPI/service/clubRoom/ClubRoomLogServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.example.iptimeAPI.service.clubRoom;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class ClubRoomLogServiceImplTest {
+
+    @Autowired
+    private ClubRoomLogService clubRoomLogService;
+
+    /*
+    날짜에 따른 데이터 조정 필요
+    (id, 유효 데이터, 비유효 데이터) = (1,1), (2,2,1), (3,1), (4,1)
+     */
+    @Test
+    public void calculateRanking_1() {
+        List<List<Long>> rankingList = clubRoomLogService.calculateRanking(List.of(1L, 2L, 3L, 4L));
+
+        int secondSize = rankingList.get(1)
+                .size(); // 1, 3, 4
+
+        Assertions.assertThat(secondSize)
+                .isEqualTo(3);
+    }
+
+    @Test
+    public void calculateRanking_2() {
+        List<List<Long>> rankingList = clubRoomLogService.calculateRanking(List.of(1L, 2L, 3L, 4L));
+
+        List<Long> first = rankingList.get(0);
+        Long firstMemberId = first.get(0);
+
+        Assertions.assertThat(firstMemberId)
+                .isEqualTo(2);
+    }
+}
