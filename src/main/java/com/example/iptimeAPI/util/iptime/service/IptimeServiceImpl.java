4f8959b1a19ee64953f3109d49a36cf8cc4d94b7
bellljun3395 <195850@jnu.ac.kr>
2023-01-03 02:49:07 +0900

    feat(IptimeServiceImpl): cookieValue 선언 시점 변경 및 cookieValue 세션 만료시 갱신 코드 구현

--- src/main/java/com/example/iptimeAPI/util/iptime/service/IptimeServiceImpl.java
+++ src/main/java/com/example/iptimeAPI/util/iptime/service/IptimeServiceImpl.java
@@ -1,23 +1,31 @@
 package com.example.iptimeAPI.util.iptime.service;
 
 import com.example.iptimeAPI.util.iptime.Iptime;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class IptimeServiceImpl implements IptimeService {
 
     private final Iptime iptime;
+    private String cookieValue;
+
+    @Autowired
+    public IptimeServiceImpl(Iptime iptime) throws IOException {
+        this.iptime = iptime;
+        this.cookieValue = iptime.getCookieValue();
+    }
 
     @Override
     public List<String> getMacAddressesList() throws IOException {
-        String cookieValue = iptime.getCookieValue();
-        iptime.login(cookieValue);
+        List<String> list = iptime.getList(cookieValue);
+        if (!list.isEmpty()) {
+            return list;
+        }
+        this.cookieValue = iptime.getCookieValue();
         return iptime.getList(cookieValue);
     }
-
 }
