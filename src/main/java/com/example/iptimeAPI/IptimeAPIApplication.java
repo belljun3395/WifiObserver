94e8034065d5debd4a21e3b1881b5ee390de45e4
bellljun3395 <195850@jnu.ac.kr>
2023-01-03 13:19:29 +0900

    feat(IptimeAPIApplication): @EnableScheduling 추가

--- src/main/java/com/example/iptimeAPI/IptimeAPIApplication.java
+++ src/main/java/com/example/iptimeAPI/IptimeAPIApplication.java
@@ -2,9 +2,11 @@ package com.example.iptimeAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class IptimeAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(IptimeAPIApplication.class, args);
