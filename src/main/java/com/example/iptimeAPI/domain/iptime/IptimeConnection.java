d039596e8ab34858e7a35d7cee32eebd9e78fdf9
belljun3395 <195850@jnu.ac.kr>
2023-01-16 00:05:51 +0900

    refactor(iptimeConnection): CommonSetting 내부 클래스로 변경 및 로그인 로직 삭제

--- src/main/java/com/example/iptimeAPI/domain/iptime/IptimeConnection.java
+++ src/main/java/com/example/iptimeAPI/domain/iptime/IptimeConnection.java
@@ -1,9 +1,10 @@
 package com.example.iptimeAPI.domain.iptime;
 
-import com.example.iptimeAPI.config.iptime.CommonSetting;
-import com.example.iptimeAPI.config.iptime.IptimeConfigAdmin;
-import com.example.iptimeAPI.config.iptime.IptimeConfig;
-import com.example.iptimeAPI.config.iptime.IptimeConfigHTTP;
+import com.example.iptimeAPI.config.iptime.IptimeAdminConfig;
+import com.example.iptimeAPI.config.iptime.info.IptimeInfoConfig;
+import com.example.iptimeAPI.config.iptime.IptimeHTTPConfig;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 
@@ -16,40 +17,31 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class IptimeConnection {
 
-    private final IptimeConfig iptimeConfig;
-    private final IptimeConfigHTTP iptimeConfigHTTP;
-    private final IptimeConfigAdmin iptimeConfigAdmin;
+    private final IptimeInfoConfig iptimeInfoConfig;
+    private final IptimeHTTPConfig iptimeHTTPConfig;
+    private final IptimeAdminConfig iptimeAdminConfig;
+    private CommonSetting commonsetting = CommonSetting.builder()
+            .agent(iptimeHTTPConfig.getUseragent())
+            .accept(iptimeHTTPConfig.getAccept())
+            .accept_encoding(iptimeHTTPConfig.getAccept_encoding())
+            .accept_language(iptimeHTTPConfig.getAccept_language())
+            .cache_control(iptimeHTTPConfig.getCache_control())
+            .connection(iptimeHTTPConfig.getConnection())
+            .host(iptimeInfoConfig.getHost())
+            .origin(iptimeInfoConfig.getOrigin())
+            .upgrade_insecure_request(iptimeHTTPConfig.getUpgrade_insecure_request())
+            .build();
 
     public String getIp() {
-        return iptimeConfig.getIp();
+        return iptimeInfoConfig.getIp();
     }
-
-
-    private CommonSetting getCommonSetting() {
-        return CommonSetting.builder()
-                .agent(iptimeConfigHTTP.getUseragent())
-                .accept(iptimeConfigHTTP.getAccept())
-                .accept_encoding(iptimeConfigHTTP.getAccept_encoding())
-                .accept_language(iptimeConfigHTTP.getAccept_language())
-                .cache_control(iptimeConfigHTTP.getCache_control())
-                .connection(iptimeConfigHTTP.getConnection())
-                .host(iptimeConfig.getHost())
-                .origin(iptimeConfig.getOrigin())
-                .upgrade_insecure_request(iptimeConfigHTTP.getUpgrade_insecure_request())
-                .build();
-    }
-
 
     public Response getCookieValue() throws IOException {
-        return connect(iptimeConfigHTTP.get_cookie_value(), Method.POST, getCommonSetting(), iptimeConfigHTTP.get_cookie_value_referer(), iptimeConfigHTTP.getContent_length(), iptimeConfigHTTP.getContent_type());
+        return connect(iptimeHTTPConfig.get_cookie_value(), Method.POST, commonsetting, iptimeHTTPConfig.get_cookie_value_referer(), iptimeHTTPConfig.getContent_length(), iptimeHTTPConfig.getContent_type());
     }
 
-//    public Response login(String cookie_value) throws IOException {
-//        return connect(iptimeConfigHTTP.get_login_url(), Method.GET, getCommonSetting(), iptimeConfigHTTP.get_login_referer(), cookie_value, iptimeConfigHTTP.getContent_length(), iptimeConfigHTTP.getContent_type());
-//    }
-
     public Response getList(String cookie_value) throws IOException {
-        return connect(iptimeConfigHTTP.get_list_url(), Method.GET, getCommonSetting(), iptimeConfigHTTP.get_list_referer(), cookie_value);
+        return connect(iptimeHTTPConfig.get_list_url(), Method.GET, commonsetting, iptimeHTTPConfig.get_list_referer(), cookie_value);
     }
 
     private Response connect(String url, Method method, CommonSetting commonSetting, String referer, String content_length, String content_type) throws IOException {
@@ -68,28 +60,7 @@ public class IptimeConnection {
                 .header("Referer", referer)
                 .header("Content-Length", content_length)
                 .header("Content-Type", content_type)
-                .data(iptimeConfigAdmin.getLoginData())
-                .execute();
-    }
-
-    private Response connect(String url, Method method, CommonSetting commonSetting, String referer, String cookie_value, String content_length, String content_type) throws IOException {
-        return Jsoup.connect(url)
-                .userAgent(commonSetting.getAgent())
-                .header("Accept", commonSetting.getAccept())
-                .header("Accept-Encoding", commonSetting.getAccept_encoding())
-                .header("Accept-Language", commonSetting.getAccept_language())
-                .header("Cache-Control", commonSetting.getCache_control())
-                .header("Connection", commonSetting.getConnection())
-                .header("Host", commonSetting.getHost())
-                .header("Origin", commonSetting.getOrigin())
-                .header("Upgrade-Insecure-Request", commonSetting.getUpgrade_insecure_request())
-                .method(method)
-
-                .header("Referer", referer)
-                .cookie("efm_session_id", cookie_value)
-                .header("Content-Length", content_length)
-                .header("Content-Type", content_type)
-                .data(iptimeConfigAdmin.getLoginData())
+                .data(iptimeAdminConfig.getLoginData())
                 .execute();
     }
 
@@ -108,7 +79,34 @@ public class IptimeConnection {
 
                 .header("Referer", referer)
                 .cookie("efm_session_id", cookie_value)
-                .data(iptimeConfigAdmin.getLoginData())
+                .data(iptimeAdminConfig.getLoginData())
                 .execute();
+    }
+
+    @Getter
+    public class CommonSetting {
+        private String agent;
+        private String accept;
+        private String accept_encoding;
+        private String accept_language;
+        private String cache_control;
+        private String connection;
+        private String host;
+        private String origin;
+        private String upgrade_insecure_request;
+
+        @Builder
+        public CommonSetting(String agent, String accept, String accept_encoding, String accept_language, String cache_control, String connection, String host, String origin, String upgrade_insecure_request) {
+            this.agent = agent;
+            this.accept = accept;
+            this.accept_encoding = accept_encoding;
+            this.accept_language = accept_language;
+            this.cache_control = cache_control;
+            this.connection = connection;
+            this.host = host;
+            this.origin = origin;
+            this.upgrade_insecure_request = upgrade_insecure_request;
+        }
+
     }
 }
