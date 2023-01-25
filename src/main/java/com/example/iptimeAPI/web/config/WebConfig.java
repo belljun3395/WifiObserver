43713b73a8ff2802e632f3c447255503c63f1215
belljun3395 <195850@jnu.ac.kr>
2023-01-25 19:23:01 +0900

    feat: cors 간단 적용

---
+++ src/main/java/com/example/iptimeAPI/web/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.iptimeAPI.web.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000");
+    }
+
+}
