faf589eecaf5b37145b2fa35258570124d6d9fd9
belljun3395 <195850@jnu.ac.kr>
2023-01-30 01:19:32 +0900

    refactor: log 설정 수정

--- src/main/java/com/example/iptimeAPI/web/exception/ExControllerAdvice.java
+++ src/main/java/com/example/iptimeAPI/web/exception/ExControllerAdvice.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
+@Slf4j(topic = "ERROR_FILE_LOGGER")
 @RestControllerAdvice
 public class ExControllerAdvice {
 
