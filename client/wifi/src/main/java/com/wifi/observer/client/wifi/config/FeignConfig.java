9e86ee9eda2f93e0d53e47ac2fb1d3739444b0a8
belljun3395 <195850@jnu.ac.kr>
2023-08-09 21:53:56 +0900

    OBS-001 feat: FeignConfig 추가

---
+++ client/wifi/src/main/java/com/wifi/observer/client/wifi/config/FeignConfig.java
@@ -0,0 +1,23 @@
+package com.wifi.observer.client.wifi.config;
+
+import static com.wifi.observer.client.wifi.WifiClientConfig.BEAN_NAME_PREFIX;
+
+import com.wifi.observer.client.wifi.WifiClientConfig;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = WifiClientConfig.BASE_PACKAGE)
+@ImportAutoConfiguration({FeignAutoConfiguration.class, HttpClientConfiguration.class})
+public class FeignConfig {
+
+	@Bean(name = BEAN_NAME_PREFIX + "HttpMessageConverters")
+	HttpMessageConverters httpMessageConverters() {
+		return new HttpMessageConverters();
+	}
+}
