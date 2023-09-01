7ede0ed31264013772bec3a172db1bfed9b78d7c
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:17:58 +0900

    OBS-006 feat: ValidatedMemberService 구현

---
+++ app/src/main/java/com/wifi/obs/app/domain/service/member/ValidatedMemberService.java
@@ -0,0 +1,24 @@
+package com.wifi.obs.app.domain.service.member;
+
+import com.wifi.obs.data.mysql.config.JpaDataSourceConfig;
+import com.wifi.obs.data.mysql.entity.member.MemberEntity;
+import com.wifi.obs.data.mysql.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ValidatedMemberService {
+
+	private final MemberRepository memberRepository;
+
+	@Transactional(transactionManager = JpaDataSourceConfig.TRANSACTION_MANAGER_NAME, readOnly = true)
+	public MemberEntity execute(Long memberId) {
+		return memberRepository
+				.findById(memberId)
+				.orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
+	}
+}
