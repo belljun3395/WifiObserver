e9158b286b67f3ee20bc8ac449e9eaeee0cf3009
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:18:57 +0900

    OBS-006 feat: DeleteDeviceRequest 구현

---
+++ app/src/main/java/com/wifi/obs/app/web/dto/request/device/DeleteDeviceRequest.java
@@ -0,0 +1,18 @@
+package com.wifi.obs.app.web.dto.request.device;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DeleteDeviceRequest {
+
+	@NotNull private Long deviceId;
+}
