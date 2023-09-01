36202859dfba487985488bfe2dcc9b2d173adb4c
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:30:03 +0900

    OBS-006 feat: DeviceInfo 구현

---
+++ app/src/main/java/com/wifi/obs/app/domain/dto/response/device/DeviceInfo.java
@@ -0,0 +1,19 @@
+package com.wifi.obs.app.domain.dto.response.device;
+
+import com.wifi.obs.data.mysql.entity.device.DeviceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class DeviceInfo {
+	private Long id;
+	private DeviceType type;
+	private String mac;
+}
