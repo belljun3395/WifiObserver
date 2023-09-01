27edcdd5edeaa5da635539eb6545fe35894826d6
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:07:39 +0900

    OBS-004 feat: AuthToken 구현

---
+++ app/src/main/java/com/wifi/obs/app/support/token/AuthToken.java
@@ -0,0 +1,19 @@
+package com.wifi.obs.app.support.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/** 인증 토큰 */
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AuthToken {
+
+	private String accessToken;
+	private String refreshToken;
+}
