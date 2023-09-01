44268cfc1e3266383e5af510eef1cac51398b0fa
belljun3395 <195850@jnu.ac.kr>
2023-09-01 15:15:20 +0900

    OBS-002 feat: ConnectHistoryMetaEntity 구현

---
+++ data/mysql/src/main/java/com/wifi/obs/data/mysql/entity/meta/ConnectHistoryMetaEntity.java
@@ -0,0 +1,66 @@
+package com.wifi.obs.data.mysql.entity.meta;
+
+import static com.wifi.obs.data.mysql.entity.meta.ConnectHistoryMetaEntity.ENTITY_PREFIX;
+
+import com.wifi.obs.data.mysql.entity.BaseEntity;
+import com.wifi.obs.data.mysql.entity.device.DeviceEntity;
+import com.wifi.obs.data.mysql.entity.wifi.service.WifiServiceEntity;
+import javax.persistence.*;
+import lombok.*;
+import lombok.ToString.Exclude;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity(name = ENTITY_PREFIX + "_entity")
+@EntityListeners({AuditingEntityListener.class})
+@Table(
+		name = ENTITY_PREFIX + "_tb",
+		indexes = {
+			@Index(
+					name = "idx_wifi_service_id_device_id_day",
+					columnList = "wifi_service_id, device_id, connect_history_meta_day"),
+			@Index(
+					name = "idx_wifi_service_id_device_id_month",
+					columnList = "wifi_service_id, device_id, connect_history_meta_month")
+		})
+@SQLDelete(sql = "UPDATE connect_history_meta_tb SET deleted=true WHERE id = ?")
+public class ConnectHistoryMetaEntity extends BaseEntity {
+
+	public static final String ENTITY_PREFIX = "connect_history_meta";
+
+	@Column(name = ENTITY_PREFIX + "_time_day", nullable = false, updatable = false)
+	private Long connectedTimeOnDay;
+
+	@Column(name = ENTITY_PREFIX + "_time_month", nullable = false, updatable = false)
+	private Long connectedTimeOnMonth;
+
+	@Column(name = ENTITY_PREFIX + "_month", updatable = false)
+	private Long month;
+
+	@Column(name = ENTITY_PREFIX + "_day", nullable = false, updatable = false)
+	private Long day;
+
+	@Exclude
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(
+			name = DeviceEntity.ENTITY_PREFIX + "_id",
+			nullable = false,
+			foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT),
+			updatable = false)
+	private DeviceEntity device;
+
+	@Exclude
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(
+			name = WifiServiceEntity.ENTITY_PREFIX + "_id",
+			nullable = false,
+			foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT),
+			updatable = false)
+	private WifiServiceEntity wifiService;
+}
