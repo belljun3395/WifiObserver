b99e852ef90b046eff15972234ab33e16c1c0597
belljun3395 <195850@jnu.ac.kr>
2023-09-01 15:14:23 +0900

    OBS-002 feat: WifiAuthEntity 구현

---
+++ data/mysql/src/main/java/com/wifi/obs/data/mysql/entity/wifi/auth/WifiAuthEntity.java
@@ -0,0 +1,33 @@
+package com.wifi.obs.data.mysql.entity.wifi.auth;
+
+import static com.wifi.obs.data.mysql.entity.wifi.auth.WifiAuthEntity.ENTITY_PREFIX;
+
+import com.wifi.obs.data.mysql.entity.BaseEntity;
+import javax.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity(name = ENTITY_PREFIX + "_entity")
+@EntityListeners({AuditingEntityListener.class})
+@Table(name = ENTITY_PREFIX + "_tb")
+@SQLDelete(sql = "UPDATE wifi_auth_tb SET deleted=true WHERE id = ?")
+public class WifiAuthEntity extends BaseEntity {
+
+	public static final String ENTITY_PREFIX = "wifi_auth";
+
+	@Column(name = ENTITY_PREFIX + "_host", nullable = false)
+	private String host;
+
+	@Column(name = ENTITY_PREFIX + "_certification", nullable = false)
+	private String certification;
+
+	@Column(name = ENTITY_PREFIX + "_password", nullable = false)
+	private String password;
+}
