1b59caa5c38de37e6680eb46a178a61844a14da2
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:20:28 +0900

    OBS-006 feat: ValidatedWifiServiceService 구현

---
+++ app/src/main/java/com/wifi/obs/app/domain/service/wifi/ValidatedWifiServiceService.java
@@ -0,0 +1,24 @@
+package com.wifi.obs.app.domain.service.wifi;
+
+import com.wifi.obs.data.mysql.config.JpaDataSourceConfig;
+import com.wifi.obs.data.mysql.entity.wifi.service.WifiServiceEntity;
+import com.wifi.obs.data.mysql.repository.wifi.service.WifiServiceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ValidatedWifiServiceService {
+
+	private final WifiServiceRepository wifiServiceRepository;
+
+	@Transactional(transactionManager = JpaDataSourceConfig.TRANSACTION_MANAGER_NAME, readOnly = true)
+	public WifiServiceEntity execute(Long wifiServiceId) {
+		return wifiServiceRepository
+				.findById(wifiServiceId)
+				.orElseThrow(() -> new RuntimeException("존재하지 않는 서비스입니다."));
+	}
+}
