2017bbc0dd0efe13c3bcc6270a92dd6c76b70af2
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:06:21 +0900

    OBS-004 refactor: app 모듈 구현을 위한 설정 임포트 추가

--- app/src/main/java/com/wifi/obs/app/config/AppConfig.java
+++ app/src/main/java/com/wifi/obs/app/config/AppConfig.java
@@ -3,13 +3,14 @@ package com.wifi.obs.app.config;
 import com.wifi.obs.data.mysql.MysqlConfig;
 import com.wifi.obs.infra.batch.BatchConfig;
 import com.wifi.obs.infra.flyway.FlywayConfig;
+import com.wifi.observer.client.wifi.WifiClientConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = AppConfig.BASE_PACKAGE)
-@Import(value = {MysqlConfig.class, FlywayConfig.class, BatchConfig.class})
+@Import(value = {MysqlConfig.class, FlywayConfig.class, BatchConfig.class, WifiClientConfig.class})
 public class AppConfig {
 
 	public static final String BASE_PACKAGE = "com.wifi.obs.app";
