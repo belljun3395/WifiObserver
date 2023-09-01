de10ff529ffe601fd08828669a073a4ac1b35e68
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:27:46 +0900

    OBS-007 feat: DeleteServiceRequest 구현

---
+++ app/src/main/java/com/wifi/obs/app/web/dto/request/service/DeleteServiceRequest.java
@@ -0,0 +1,18 @@
+package com.wifi.obs.app.web.dto.request.service;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DeleteServiceRequest {
+
+	@NotNull Long sid;
+}
