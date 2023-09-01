03776becd14b20feec9eec9e3764ae9911a17ba7
belljun3395 <195850@jnu.ac.kr>
2023-09-01 17:16:51 +0900

    OBS-006 feat: SaveDeviceRequest

---
+++ app/src/main/java/com/wifi/obs/app/web/dto/request/device/SaveDeviceRequest.java
@@ -0,0 +1,24 @@
+package com.wifi.obs.app.web.dto.request.device;
+
+import com.wifi.obs.data.mysql.entity.device.DeviceType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class SaveDeviceRequest {
+
+	@NotNull private Long sid;
+	@NotNull private DeviceType deviceType;
+
+	@Pattern(regexp = "([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}")
+	private String mac;
+}
