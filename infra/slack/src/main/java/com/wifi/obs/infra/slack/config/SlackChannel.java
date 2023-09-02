0216440638f444674b98deb8b458fb1dddea598f
belljun3395 <195850@jnu.ac.kr>
2023-09-02 16:34:57 +0900

    OBS-010 refactor: SlackChannel 요소 추가
    메시지를 더 풍성하게 하기 위한 타입과, 이유를 추가 하였습니다.

--- infra/slack/src/main/java/com/wifi/obs/infra/slack/config/SlackChannel.java
+++ infra/slack/src/main/java/com/wifi/obs/infra/slack/config/SlackChannel.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum SlackChannel {
-	ERROR("#장애"),
-	BATCH("#배치");
+	ERROR("#장애", "장애", "장애가 발생하였습니다."),
+	BATCH("#배치", "배치", "배치가 정상적으로 동작 중입니다.");
 
 	private final String channelName;
+	private final String type;
+	private final String reason;
 
-	SlackChannel(String channelName) {
+	SlackChannel(String channelName, String type, String reason) {
 		this.channelName = channelName;
+		this.type = type;
+		this.reason = reason;
 	}
 }
