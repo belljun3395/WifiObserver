6c6a37f181430d063a4ce148e19df2155a18d886
belljun3395 <195850@jnu.ac.kr>
2023-09-02 16:35:56 +0900

    OBS-010 refactor: Slack Block Kit Builder를 활용하기 위한 수정

--- infra/slack/src/main/java/com/wifi/obs/infra/slack/service/SlackService.java
+++ infra/slack/src/main/java/com/wifi/obs/infra/slack/service/SlackService.java
@@ -3,9 +3,14 @@ package com.wifi.obs.infra.slack.service;
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.model.block.Blocks;
+import com.slack.api.model.block.SectionBlock;
+import com.slack.api.model.block.composition.MarkdownTextObject;
+import com.slack.api.model.block.composition.TextObject;
 import com.wifi.obs.infra.slack.config.SlackChannel;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,15 +25,40 @@ public class SlackService {
 	public void sendSlackMessage(String message, SlackChannel channel) {
 		MethodsClient methods = Slack.getInstance().methods(token);
 
-		ChatPostMessageRequest request =
-				ChatPostMessageRequest.builder().channel(channel.getChannelName()).text(message).build();
-
 		try {
-			methods.chatPostMessage(request);
+			methods.chatPostMessage(
+					req ->
+							req.channel(channel.getChannelName())
+									.blocks(
+											Blocks.asBlocks(
+													getMessageSectionBlock(message),
+													getDefaultAndMessageTypeSectionBlock(channel))));
 		} catch (SlackApiException | IOException e) {
 			log.error(e.getMessage());
 		}
 
 		log.info("Slack message sent. Channel: {}, Message: {}", channel.getChannelName(), message);
+	}
+
+	public static SectionBlock getMessageSectionBlock(String message) {
+		return SectionBlock.builder().text(MarkdownTextObject.builder().text(message).build()).build();
+	}
+
+	public static SectionBlock getDefaultAndMessageTypeSectionBlock(SlackChannel channel) {
+		return SectionBlock.builder()
+				.fields(List.of(typeTextObject(channel), whenTexObject(), reasonTextObject(channel)))
+				.build();
+	}
+
+	public static TextObject typeTextObject(SlackChannel channel) {
+		return MarkdownTextObject.builder().text("*Type:*\n" + channel.getType()).build();
+	}
+
+	public static TextObject whenTexObject() {
+		return MarkdownTextObject.builder().text("*When:*\n" + LocalDateTime.now()).build();
+	}
+
+	public static TextObject reasonTextObject(SlackChannel channel) {
+		return MarkdownTextObject.builder().text("*Reason:*\n" + channel.getReason()).build();
 	}
 }
