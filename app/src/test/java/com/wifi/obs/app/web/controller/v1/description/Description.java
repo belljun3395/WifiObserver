2d8f63f0e2387465cae51060655cbbda875a72ce
belljun3395 <195850@jnu.ac.kr>
2023-09-03 11:53:43 +0900

    OBS-011 feat: authHeader Description 추가

--- app/src/test/java/com/wifi/obs/app/web/controller/v1/description/Description.java
+++ app/src/test/java/com/wifi/obs/app/web/controller/v1/description/Description.java
@@ -1,7 +1,9 @@
 package com.wifi.obs.app.web.controller.v1.description;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
+import com.epages.restdocs.apispec.HeaderDescriptorWithType;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -18,5 +20,11 @@ public class Description {
 
 	public static FieldDescriptor[] success(FieldDescriptor[] data) {
 		return ArrayUtils.addAll(data, getSuccessMessageDescriptor(), getSuccessCodeDescriptor());
+	}
+
+	public static HeaderDescriptorWithType authHeader() {
+		return headerWithName("Authorization")
+				.defaultValue("{{accessToken}}")
+				.description("Bearer 어세스 토큰");
 	}
 }
