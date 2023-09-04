c9d0eeec78027ffa44e79f335cca6a2ac281d1c0
belljun3395 <195850@jnu.ac.kr>
2023-09-04 15:16:57 +0900

    OBS-015 feat: GetMonthServiceDeviceStetInfos 구현

---
+++ app/src/main/java/com/wifi/obs/app/domain/service/device/GetMonthServiceDeviceStetInfos.java
@@ -0,0 +1,48 @@
+package com.wifi.obs.app.domain.service.device;
+
+import com.wifi.obs.app.domain.dto.response.device.DeviceStetInfo;
+import com.wifi.obs.app.domain.dto.response.service.ServiceDeviceStetInfos;
+import com.wifi.obs.data.mysql.entity.device.DeviceEntity;
+import com.wifi.obs.data.mysql.entity.meta.ConnectHistoryMetaEntity;
+import com.wifi.obs.data.mysql.repository.meta.ConnectHistoryMetaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GetMonthServiceDeviceStetInfos implements GetServiceDeviceStetInfos {
+
+	private final ConnectHistoryMetaRepository connectHistoryMetaRepository;
+
+	@Override
+	public ServiceDeviceStetInfos execute(
+			List<DeviceEntity> devices,
+			List<DeviceStetInfo> deviceStetInfos,
+			Long serviceId,
+			LocalDateTime now) {
+		for (DeviceEntity device : devices) {
+			Optional<ConnectHistoryMetaEntity> monthStet =
+					connectHistoryMetaRepository.findTopByDeviceAndMonthOrderByIdDesc(
+							device, (long) now.getMonth().getValue());
+
+			if (monthStet.isEmpty()) {
+				DeviceStetInfo stetInfo =
+						DeviceStetInfo.builder().id(device.getId()).mac(device.getMac()).build();
+				deviceStetInfos.add(stetInfo);
+				continue;
+			}
+
+			DeviceStetInfo stetInfo =
+					DeviceStetInfo.builder()
+							.id(device.getId())
+							.mac(device.getMac())
+							.time(monthStet.get().getConnectedTimeOnMonth())
+							.build();
+			deviceStetInfos.add(stetInfo);
+		}
+		return new ServiceDeviceStetInfos(serviceId, deviceStetInfos);
+	}
+}
