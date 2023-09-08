31151eb2ca2451bee2bac0a3e3ce85b8f6f1a0f0
belljun3395 <195850@jnu.ac.kr>
2023-09-08 13:03:19 +0900

    OBS-017 test: GetOnConnectDeviceFacadeUseCaseTest 구현

---
+++ app/src/test/java/com/wifi/obs/app/domain/usecase/device/GetOnConnectDeviceFacadeUseCaseTest.java
@@ -0,0 +1,52 @@
+package com.wifi.obs.app.domain.usecase.device;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.wifi.obs.app.config.AppConfig;
+import com.wifi.obs.app.domain.dto.response.service.OnConnectUserInfos;
+import com.wifi.obs.app.domain.dto.response.service.UserInfo;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+@Slf4j
+@ActiveProfiles("test")
+@SpringBootTest
+@ContextConfiguration(classes = {AppConfig.class})
+class GetOnConnectDeviceFacadeUseCaseTest {
+
+	@Autowired GetOnConnectDeviceFacadeUseCase getOnConnectDeviceFacadeUseCase;
+
+	static String mac = "00:00:00:00:00:00";
+
+	@Test
+	void getDeviceOnConnectInfo_on_connect() {
+		// given
+		OnConnectUserInfos connectUserInfos =
+				OnConnectUserInfos.of(List.of(UserInfo.builder().mac(mac).build()));
+
+		// when
+		Optional<String> res = getOnConnectDeviceFacadeUseCase.getFilteredMac(mac, connectUserInfos);
+
+		// then
+		assertThat(res).isPresent();
+		assertThat(res.get()).contains(mac);
+	}
+
+	@Test
+	void getDeviceOnConnectInfo_disconnect() {
+		// given
+		OnConnectUserInfos connectUserInfos = OnConnectUserInfos.of(List.of());
+
+		// when
+		Optional<String> res = getOnConnectDeviceFacadeUseCase.getFilteredMac(mac, connectUserInfos);
+
+		// then
+		assertThat(res).isEmpty();
+	}
+}
