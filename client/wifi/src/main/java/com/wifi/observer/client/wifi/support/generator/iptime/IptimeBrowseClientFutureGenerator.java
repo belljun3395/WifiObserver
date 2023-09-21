a9bc412eb224ee2bf0b1245978aa419e28c73915
belljun3395 <195850@jnu.ac.kr>
2023-09-21 09:48:49 +0900

    OBS-019 refactor: Generator 클래스에 변경된 인터페이스 적용 및 메서드 분리

--- client/wifi/src/main/java/com/wifi/observer/client/wifi/support/generator/iptime/IptimeBrowseClientFutureGenerator.java
+++ client/wifi/src/main/java/com/wifi/observer/client/wifi/support/generator/iptime/IptimeBrowseClientFutureGenerator.java
@@ -1,12 +1,18 @@
 package com.wifi.observer.client.wifi.support.generator.iptime;
 
 import com.wifi.observer.client.wifi.dto.http.IptimeWifiBrowseClientDto;
+import com.wifi.observer.client.wifi.dto.response.ClientResponse;
+import com.wifi.observer.client.wifi.dto.response.OnConnectUserInfos;
 import com.wifi.observer.client.wifi.dto.response.iptime.IptimeOnConnectUserInfosResponse;
 import com.wifi.observer.client.wifi.http.request.get.BrowseClientQuery;
+import com.wifi.observer.client.wifi.model.BrowseQueryClientModel;
 import com.wifi.observer.client.wifi.support.converter.iptime.IptimeBrowseConverter;
 import com.wifi.observer.client.wifi.util.resolver.users.IptimeUsersOnConnectFilterDecorator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -31,12 +37,25 @@ public class IptimeBrowseClientFutureGenerator {
 		this.requestAsyncExecutor = requestAsyncExecutor;
 	}
 
-	public CompletableFuture<IptimeOnConnectUserInfosResponse> execute(
+	public CompletableFuture<ClientResponse<OnConnectUserInfos>> execute(
 			IptimeWifiBrowseClientDto queryDto) {
 		final String host = queryDto.getHost();
-		return CompletableFuture.supplyAsync(
-						() -> browseClientQuery.query(queryDto), requestAsyncExecutor)
-				.thenApply(onConnectUsersResolver::resolve)
-				.thenApply(users -> iptimeBrowseClientConverter.from(users, host));
+		return CompletableFuture.supplyAsync(getQueryResponseSupplier(queryDto), requestAsyncExecutor)
+				.thenApply(getResolveResponseFunction())
+				.thenApply(getResponseFunction(host));
+	}
+
+	private Supplier<BrowseQueryClientModel> getQueryResponseSupplier(
+			IptimeWifiBrowseClientDto queryDto) {
+		return () -> browseClientQuery.query(queryDto);
+	}
+
+	private Function<BrowseQueryClientModel, List<String>> getResolveResponseFunction() {
+		return response -> onConnectUsersResolver.resolve(response.getUsersInfo());
+	}
+
+	private Function<List<String>, IptimeOnConnectUserInfosResponse> getResponseFunction(
+			String host) {
+		return users -> iptimeBrowseClientConverter.from(users, host);
 	}
 }
--- client/wifi/src/main/java/com/wifi/observer/client/wifi/support/generator/iptime/IptimeHealthFutureGenerator.java
+++ client/wifi/src/main/java/com/wifi/observer/client/wifi/support/generator/iptime/IptimeHealthFutureGenerator.java
@@ -1,12 +1,17 @@
 package com.wifi.observer.client.wifi.support.generator.iptime;
 
 import com.wifi.observer.client.wifi.dto.http.IptimeWifiHealthClientDto;
+import com.wifi.observer.client.wifi.dto.response.ClientResponse;
 import com.wifi.observer.client.wifi.dto.response.common.CommonHealthStatusResponse;
 import com.wifi.observer.client.wifi.http.request.get.HealthClientQuery;
+import com.wifi.observer.client.wifi.model.HealthQueryClientModel;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -23,11 +28,20 @@ public class IptimeHealthFutureGenerator {
 		this.healthAsyncExecutor = healthAsyncExecutor;
 	}
 
-	public CompletableFuture<CommonHealthStatusResponse> execute(IptimeWifiHealthClientDto queryDto) {
+	public CompletableFuture<ClientResponse<HttpStatus>> execute(IptimeWifiHealthClientDto queryDto) {
 		final String host = queryDto.getHost();
-		return CompletableFuture.supplyAsync(
-						() -> healthClientQuery.query(queryDto), healthAsyncExecutor)
-				.thenApply(
-						status -> CommonHealthStatusResponse.builder().response(status).host(host).build());
+		return CompletableFuture.supplyAsync(getQueryResponseSupplier(queryDto), healthAsyncExecutor)
+				.thenApply(getResponseFunction(host));
+	}
+
+	private Supplier<HealthQueryClientModel> getQueryResponseSupplier(
+			IptimeWifiHealthClientDto queryDto) {
+		return () -> healthClientQuery.query(queryDto);
+	}
+
+	private Function<HealthQueryClientModel, CommonHealthStatusResponse> getResponseFunction(
+			String host) {
+		return response ->
+				CommonHealthStatusResponse.builder().response(response.getHttpStatus()).host(host).build();
 	}
 }
