be727be1b21281950584976899ee520bcc29f681
belljun3395 <195850@jnu.ac.kr>
2023-09-21 09:24:20 +0900

    OBS-019 feat: DocumentInfo 선언

---
+++ client/wifi/src/main/java/com/wifi/observer/client/wifi/model/info/DocumentInfo.java
@@ -0,0 +1,5 @@
+package com.wifi.observer.client.wifi.model.info;
+
+import com.wifi.observer.client.wifi.support.jsoup.ClientDocument;
+
+public interface DocumentInfo extends ClientHttpInfo<ClientDocument> {}
