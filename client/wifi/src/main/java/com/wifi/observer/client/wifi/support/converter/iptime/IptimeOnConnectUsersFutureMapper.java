26e59386d4be04f2dcf4a29ada867b7f03d96ff5
belljun3395 <195850@jnu.ac.kr>
2023-09-21 09:48:18 +0900

    OBS-019 refactor: FutureMapper 클래스에 변경된 인터페이스 적용

--- client/wifi/src/main/java/com/wifi/observer/client/wifi/support/converter/iptime/IptimeOnConnectUsersFutureMapper.java
+++ client/wifi/src/main/java/com/wifi/observer/client/wifi/support/converter/iptime/IptimeOnConnectUsersFutureMapper.java
@@ -1,6 +1,7 @@
 package com.wifi.observer.client.wifi.support.converter.iptime;
 
-import com.wifi.observer.client.wifi.dto.response.iptime.IptimeOnConnectUserInfosResponse;
+import com.wifi.observer.client.wifi.dto.response.ClientResponse;
+import com.wifi.observer.client.wifi.dto.response.OnConnectUserInfos;
 import com.wifi.observer.client.wifi.exception.WifiCompletionException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class IptimeOnConnectUsersFutureMapper {
 
-	public IptimeOnConnectUserInfosResponse from(
-			CompletableFuture<IptimeOnConnectUserInfosResponse> future) {
+	public ClientResponse<OnConnectUserInfos> from(
+			CompletableFuture<ClientResponse<OnConnectUserInfos>> future) {
 		try {
 			return future.join();
 		} catch (CompletionException e) {
