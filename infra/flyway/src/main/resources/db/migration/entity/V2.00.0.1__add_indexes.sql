9bead63bdc0adcb161afca50932e6b3e888bbead
belljun3395 <195850@jnu.ac.kr>
2023-10-02 13:42:51 +0900

    OBS-020 fix: 인덱스 명 오타 주석 추가

--- infra/flyway/src/main/resources/db/migration/entity/V2.00.0.1__add_indexes.sql
+++ infra/flyway/src/main/resources/db/migration/entity/V2.00.0.1__add_indexes.sql
@@ -1,6 +1,7 @@
 create index idx_member_certification on member_tb (member_certification);
 
 create index idx_member_id on wifi_service_tb (member_id);
+# 오타로 인해 사용되지 않는 인덱스
 create index idx_wifi_staus on wifi_service_tb (wifi_service_status);
 
 create index idx_wifi_service_id_device_mac on device_tb (wifi_service_id, device_mac);
