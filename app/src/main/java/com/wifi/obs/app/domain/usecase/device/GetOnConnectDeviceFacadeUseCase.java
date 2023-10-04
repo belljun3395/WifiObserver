e76e28be7e09485563232076f12570da73540574
belljun3395 <195850@jnu.ac.kr>
2023-10-04 15:00:17 +0900

    OBS-022 refactor: device usecase 클래스에 data 계층과 연관 끊고 도메인 사용하도록 수정

--- app/src/main/java/com/wifi/obs/app/domain/usecase/device/GetOnConnectDeviceFacadeUseCase.java
+++ app/src/main/java/com/wifi/obs/app/domain/usecase/device/GetOnConnectDeviceFacadeUseCase.java
@@ -1,24 +1,20 @@
 package com.wifi.obs.app.domain.usecase.device;
 
-import com.wifi.obs.app.domain.converter.DeviceModelConverter;
-import com.wifi.obs.app.domain.converter.MemberModelConverter;
-import com.wifi.obs.app.domain.converter.WifiServiceModelConverter;
+import com.wifi.obs.app.domain.converter.MemberConverter;
+import com.wifi.obs.app.domain.converter.WifiServiceDeviceConverter;
 import com.wifi.obs.app.domain.dto.response.device.DeviceOnConnectInfo;
 import com.wifi.obs.app.domain.dto.response.service.OnConnectUserInfos;
 import com.wifi.obs.app.domain.dto.response.service.UserInfo;
-import com.wifi.obs.app.domain.model.DeviceModel;
-import com.wifi.obs.app.domain.model.MemberModel;
-import com.wifi.obs.app.domain.model.WifiServiceModel;
+import com.wifi.obs.app.domain.model.device.Device;
+import com.wifi.obs.app.domain.model.device.WifiServiceDevice;
+import com.wifi.obs.app.domain.model.member.Member;
 import com.wifi.obs.app.domain.service.member.ValidatedMemberService;
 import com.wifi.obs.app.domain.service.wifi.GetUserService;
 import com.wifi.obs.app.domain.usecase.support.manager.GetUserServiceManager;
 import com.wifi.obs.app.domain.usecase.util.validator.IdMatchValidator;
 import com.wifi.obs.app.exception.domain.DeviceNotFoundException;
 import com.wifi.obs.data.mysql.config.JpaDataSourceConfig;
-import com.wifi.obs.data.mysql.entity.device.DeviceEntity;
 import com.wifi.obs.data.mysql.entity.support.WifiAuthEntitySupporter;
-import com.wifi.obs.data.mysql.entity.support.WifiServiceEntitySupporter;
-import com.wifi.obs.data.mysql.entity.wifi.auth.WifiAuthEntity;
 import com.wifi.obs.data.mysql.repository.device.DeviceRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -36,61 +32,66 @@ public class GetOnConnectDeviceFacadeUseCase {
 	private final ValidatedMemberService validatedMemberService;
 	private final GetUserServiceManager getUserServiceManager;
 
-	private final DeviceModelConverter deviceModelConverter;
-	private final MemberModelConverter memberModelConverter;
-	private final WifiServiceModelConverter wifiServiceModelConverter;
+	private final MemberConverter memberConverter;
+	private final WifiServiceDeviceConverter wifiServiceDeviceConverter;
 
 	private final IdMatchValidator idMatchValidator;
 
-	private final WifiServiceEntitySupporter wifiServiceEntitySupporter;
 	private final WifiAuthEntitySupporter wifiAuthEntitySupporter;
 
 	@Transactional(transactionManager = JpaDataSourceConfig.TRANSACTION_MANAGER_NAME, readOnly = true)
 	public DeviceOnConnectInfo execute(Long memberId, String mac) {
 
-		MemberModel member = memberModelConverter.from(validatedMemberService.execute(memberId));
+		Member member = getMember(memberId);
 
-		DeviceModel device = deviceModelConverter.from(getDevice(mac));
+		WifiServiceDevice device = getDevice(mac);
 
-		WifiServiceModel service =
-				wifiServiceModelConverter.from(
-						wifiServiceEntitySupporter.getReferenceEntity(device.getServiceId()));
+		idMatchValidator.validate(member.getId(), device.getMemberId());
 
-		idMatchValidator.validate(member.getId(), service.getMemberId());
+		GetUserService getUserService = getService(device);
 
-		GetUserService getUserService = getUserServiceManager.getService(service.getServiceType());
-
-		return getDeviceOnConnectInfo(
-				getUserService,
-				mac,
-				deviceModelConverter.toEntity(device),
-				wifiAuthEntitySupporter.getReferenceEntity(service.getAuthId()));
+		return getDeviceOnConnectInfo(getUserService, mac, device);
 	}
 
-	private DeviceEntity getDevice(String mac) {
-		return deviceRepository
-				.findByMacAndDeletedFalse(mac)
-				.orElseThrow(() -> new DeviceNotFoundException(mac));
+	private Member getMember(Long memberId) {
+		return memberConverter.from(validatedMemberService.execute(memberId));
+	}
+
+	private WifiServiceDevice getDevice(String mac) {
+		return wifiServiceDeviceConverter.from(
+				deviceRepository
+						.findByMacAndDeletedFalse(mac)
+						.orElseThrow(() -> new DeviceNotFoundException(mac)));
+	}
+
+	private GetUserService getService(WifiServiceDevice device) {
+		return getUserServiceManager.getService(device.getServiceType());
 	}
 
 	protected DeviceOnConnectInfo getDeviceOnConnectInfo(
-			GetUserService service, String mac, DeviceEntity device, WifiAuthEntity auth) {
+			GetUserService service, String mac, WifiServiceDevice device) {
 
-		OnConnectUserInfos users = service.execute(auth);
+		OnConnectUserInfos users = getUsers(service, device);
 
-		Optional<String> filteredMac = getFilteredMac(mac, users);
-		if (filteredMac.isEmpty()) {
+		Optional<String> filteredUsers = getFilteredUsers(mac, users);
+
+		if (filteredUsers.isEmpty()) {
 			return getDeviceOnConnectInfo(device, false);
 		}
 
 		return getDeviceOnConnectInfo(device, true);
 	}
 
-	protected Optional<String> getFilteredMac(String mac, OnConnectUserInfos users) {
-		return users.getUserInfos().stream().map(UserInfo::getMac).filter(mac::equals).findFirst();
+	private OnConnectUserInfos getUsers(GetUserService service, WifiServiceDevice device) {
+		return service.execute(
+				wifiAuthEntitySupporter.getWifiAuthIdEntity(device.getService().getAuthId()));
 	}
 
-	private DeviceOnConnectInfo getDeviceOnConnectInfo(DeviceEntity device, Boolean connected) {
+	protected Optional<String> getFilteredUsers(String mac, OnConnectUserInfos users) {
+		return users.getUsers().stream().map(UserInfo::getMac).filter(mac::equals).findFirst();
+	}
+
+	private DeviceOnConnectInfo getDeviceOnConnectInfo(Device device, Boolean connected) {
 		return DeviceOnConnectInfo.builder()
 				.id(device.getId())
 				.mac(device.getMac())
