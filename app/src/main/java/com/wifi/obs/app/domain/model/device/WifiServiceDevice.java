e50db21a33950552e8fc775448792632ef28c89b
belljun3395 <195850@jnu.ac.kr>
2023-10-04 14:54:16 +0900

    OBS-022 feat: WifiServiceDevice 구현

---
+++ app/src/main/java/com/wifi/obs/app/domain/model/device/WifiServiceDevice.java
@@ -0,0 +1,36 @@
+package com.wifi.obs.app.domain.model.device;
+
+import com.wifi.obs.app.domain.model.wifi.WifiService;
+import com.wifi.obs.app.domain.model.wifi.WifiServiceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class WifiServiceDevice extends Device {
+
+	private WifiService service;
+
+	public Long getMemberId() {
+		return service.getMemberId();
+	}
+
+	public WifiServiceType getServiceType() {
+		return service.getType();
+	}
+
+	public boolean isSameService(Long target) {
+		return service.isSameWifiService(target);
+	}
+
+	public void changeService(Long newServiceId) {
+		super.changeServiceId(newServiceId);
+		this.service = service.toBuilder().id(newServiceId).build();
+	}
+}
