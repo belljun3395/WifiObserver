19951120482f97d0610e0a2ebe2c93f20ca46d74
belljun3395 <195850@jnu.ac.kr>
2023-10-04 15:00:54 +0900

    OBS-022 refactor: app test에 변경사항 반영

--- app/src/test/java/com/wifi/obs/app/domain/usecase/wifiService/GetWifiServiceInfoUseCaseTest.java
+++ app/src/test/java/com/wifi/obs/app/domain/usecase/wifiService/GetWifiServiceInfoUseCaseTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.wifi.obs.app.config.AppConfig;
 import com.wifi.obs.app.domain.dto.response.service.WifiServiceInfos;
-import com.wifi.obs.app.domain.model.DeviceModel;
-import com.wifi.obs.app.domain.model.WifiServiceModel;
-import com.wifi.obs.data.mysql.entity.device.DeviceType;
-import com.wifi.obs.data.mysql.entity.wifi.service.WifiServiceType;
-import com.wifi.obs.data.mysql.entity.wifi.service.WifiStatus;
+import com.wifi.obs.app.domain.model.device.Device;
+import com.wifi.obs.app.domain.model.device.DeviceType;
+import com.wifi.obs.app.domain.model.wifi.WifiAuth;
+import com.wifi.obs.app.domain.model.wifi.WifiService;
+import com.wifi.obs.app.domain.model.wifi.WifiServiceType;
+import com.wifi.obs.app.domain.model.wifi.WifiStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -28,33 +29,40 @@ class GetWifiServiceInfoUseCaseTest {
 
 	static Long serviceId = 1L;
 	static Long memberId = 1L;
-	static Long authId = 1L;
+	static WifiAuth auth =
+			WifiAuth.builder()
+					.id(1L)
+					.host("host")
+					.certification("certification")
+					.password("password")
+					.build();
+
 	static Long deviceId = 1L;
 	static String mac = "00:00:00:00:00:00";
 
 	@Test
 	void getServiceInfos() {
 		// given
-		List<WifiServiceModel> services =
+		List<WifiService> services =
 				List.of(
-						WifiServiceModel.builder()
+						WifiService.builder()
 								.id(serviceId)
 								.memberId(memberId)
-								.authId(authId)
-								.serviceType(WifiServiceType.IPTIME)
+								.auth(auth)
+								.type(WifiServiceType.IPTIME)
 								.cycle(10L)
 								.standardTime(9L)
 								.status(WifiStatus.ON)
 								.createdAt(LocalDateTime.now())
 								.build());
 
-		List<List<DeviceModel>> devices =
+		List<List<Device>> devices =
 				List.of(
 						List.of(
-								DeviceModel.builder()
+								Device.builder()
 										.id(deviceId)
 										.serviceId(serviceId)
-										.deviceType(DeviceType.NOTEBOOK)
+										.type(DeviceType.NOTEBOOK)
 										.mac(mac)
 										.build()));
 
@@ -63,11 +71,10 @@ class GetWifiServiceInfoUseCaseTest {
 
 		// then
 		assertThat(res).isNotNull();
-		assertThat(res.getWifiServiceInfos()).isNotEmpty();
-		assertThat(res.getWifiServiceInfos().get(0).getDeviceInfos().get(0).getId())
-				.isEqualTo(deviceId);
-		assertThat(res.getWifiServiceInfos().get(0).getDeviceInfos().get(0).getMac()).isEqualTo(mac);
-		assertThat(res.getWifiServiceInfos().get(0).getDeviceInfos().get(0).getType())
+		assertThat(res.getServices()).isNotEmpty();
+		assertThat(res.getServices().get(0).getDevices().get(0).getId()).isEqualTo(deviceId);
+		assertThat(res.getServices().get(0).getDevices().get(0).getMac()).isEqualTo(mac);
+		assertThat(res.getServices().get(0).getDevices().get(0).getType())
 				.isEqualTo(DeviceType.NOTEBOOK);
 	}
 }
