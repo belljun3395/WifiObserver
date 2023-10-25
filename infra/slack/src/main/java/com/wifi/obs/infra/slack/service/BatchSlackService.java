301037760a8eef3549de34aae89ca9959a83833a
belljun3395 <195850@jnu.ac.kr>
2023-10-25 16:51:52 +0900

    OBS-026 feat: BatchSlackService 구현

---
+++ infra/slack/src/main/java/com/wifi/obs/infra/slack/service/BatchSlackService.java
@@ -0,0 +1,19 @@
+package com.wifi.obs.infra.slack.service;
+
+import com.wifi.obs.infra.slack.config.SlackChannel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BatchSlackService implements BatchNotificationService {
+
+	private final SlackService slackService;
+
+	@Override
+	public void sendNotification(String message) {
+		slackService.sendNotification(message, SlackChannel.BATCH);
+	}
+}
