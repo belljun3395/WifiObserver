9cfb1d9785ed10a98e5903b7313097fa40e008a2
belljun3395 <195850@jnu.ac.kr>
2023-10-31 15:33:04 +0900

    OBS-029 refactor: DeviceEntity가 아닌 Device 타입으로 받도록 수정 반영

--- app/src/main/java/com/wifi/obs/app/domain/service/device/GetMonthServiceDeviceStetInfos.java
+++ app/src/main/java/com/wifi/obs/app/domain/service/device/GetMonthServiceDeviceStetInfos.java
@@ -1,11 +1,14 @@
 package com.wifi.obs.app.domain.service.device;
 
+import com.wifi.obs.app.domain.converter.DeviceConverter;
 import com.wifi.obs.app.domain.dto.response.device.DeviceStetInfo;
 import com.wifi.obs.app.domain.dto.response.service.ServiceDeviceStetInfos;
+import com.wifi.obs.app.domain.model.device.Device;
 import com.wifi.obs.data.mysql.entity.device.DeviceEntity;
 import com.wifi.obs.data.mysql.entity.meta.ConnectHistoryMetaEntity;
 import com.wifi.obs.data.mysql.repository.meta.ConnectHistoryMetaRepository;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +19,13 @@ import org.springframework.stereotype.Component;
 public class GetMonthServiceDeviceStetInfos implements GetServiceDeviceStetInfos {
 
 	private final ConnectHistoryMetaRepository connectHistoryMetaRepository;
+	private final DeviceConverter deviceConverter;
 
 	@Override
 	public ServiceDeviceStetInfos execute(
-			List<DeviceEntity> devices, List<DeviceStetInfo> stetInfos, Long sid, LocalDateTime now) {
-		for (DeviceEntity device : devices) {
+			List<Device> devices, List<DeviceStetInfo> stetInfos, Long sid, LocalDateTime now) {
+		List<DeviceEntity> entities = convertToEntities(devices);
+		for (DeviceEntity device : entities) {
 			Optional<ConnectHistoryMetaEntity> monthStet =
 					connectHistoryMetaRepository.findTopByDeviceAndMonthOrderByIdDesc(
 							device, (long) now.getMonth().getValue());
@@ -41,5 +46,13 @@ public class GetMonthServiceDeviceStetInfos implements GetServiceDeviceStetInfos
 			stetInfos.add(stetInfo);
 		}
 		return new ServiceDeviceStetInfos(sid, stetInfos);
+	}
+
+	private List<DeviceEntity> convertToEntities(List<Device> devices) {
+		List<DeviceEntity> entities = new ArrayList<>();
+		for (Device device : devices) {
+			entities.add(deviceConverter.toEntity(device));
+		}
+		return entities;
 	}
 }
