211001cf8bd4d59cff2067a667336a3513614a18
belljun3395 <195850@jnu.ac.kr>
2023-10-31 15:32:43 +0900

    OBS-029 refactor: DeviceEntity가 아닌 Device 타입으로 받도록 수정

--- app/src/main/java/com/wifi/obs/app/domain/service/device/GetServiceDeviceStetInfos.java
+++ app/src/main/java/com/wifi/obs/app/domain/service/device/GetServiceDeviceStetInfos.java
@@ -2,12 +2,12 @@ package com.wifi.obs.app.domain.service.device;
 
 import com.wifi.obs.app.domain.dto.response.device.DeviceStetInfo;
 import com.wifi.obs.app.domain.dto.response.service.ServiceDeviceStetInfos;
-import com.wifi.obs.data.mysql.entity.device.DeviceEntity;
+import com.wifi.obs.app.domain.model.device.Device;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GetServiceDeviceStetInfos {
 
 	ServiceDeviceStetInfos execute(
-			List<DeviceEntity> devices, List<DeviceStetInfo> stetInfos, Long sid, LocalDateTime now);
+			List<Device> devices, List<DeviceStetInfo> stetInfos, Long sid, LocalDateTime now);
 }
