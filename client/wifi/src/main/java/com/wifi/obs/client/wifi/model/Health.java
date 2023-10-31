cff22303d61763cc2601c366a108582f96520a37
belljun3395 <195850@jnu.ac.kr>
2023-10-31 13:46:48 +0900

    OBS-028 refactor: Health#status 타입 HttpStatusResponse로 수정

--- client/wifi/src/main/java/com/wifi/obs/client/wifi/model/Health.java
+++ client/wifi/src/main/java/com/wifi/obs/client/wifi/model/Health.java
@@ -1,7 +1,6 @@
 package com.wifi.obs.client.wifi.model;
 
 import com.wifi.obs.client.wifi.http.HttpStatusResponse;
-import com.wifi.obs.client.wifi.model.value.HealthQueryVO;
 import java.util.Objects;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -15,22 +14,21 @@ import org.springframework.http.HttpStatus;
 @Builder(toBuilder = true)
 public class Health {
 
-	private final HealthQueryVO statusInfo;
+	private final HttpStatusResponse status;
 	private final String host;
 
 	public static Health fail(String host) {
 		return Health.builder()
-				.statusInfo(
-						HealthQueryVO.builder().info(HttpStatusResponse.of(HttpStatus.BAD_REQUEST)).build())
+				.status(HttpStatusResponse.of(HttpStatus.BAD_REQUEST))
 				.host(host)
 				.build();
 	}
 
 	public boolean isFail() {
-		return Objects.equals(statusInfo.getInfo(), HttpStatusResponse.of(HttpStatus.BAD_REQUEST));
+		return Objects.equals(status, HttpStatusResponse.of(HttpStatus.BAD_REQUEST));
 	}
 
 	public HttpStatusResponse getHttpStatus() {
-		return statusInfo.getInfo();
+		return status;
 	}
 }
