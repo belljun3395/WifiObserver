fe8df69930ad653d9fbd8dd449439790c5c6c283
belljun3395 <195850@jnu.ac.kr>
2023-10-31 13:45:08 +0900

    OBS-028 refactor: HttpResponse#query 반환 수정

--- client/wifi/src/main/java/com/wifi/obs/client/wifi/http/request/get/HealthClientQuery.java
+++ client/wifi/src/main/java/com/wifi/obs/client/wifi/http/request/get/HealthClientQuery.java
@@ -3,14 +3,13 @@ package com.wifi.obs.client.wifi.http.request.get;
 import com.wifi.obs.client.wifi.dto.http.WifiHealthRequestElement;
 import com.wifi.obs.client.wifi.exception.WifiURISyntaxException;
 import com.wifi.obs.client.wifi.http.HttpStatusResponse;
-import com.wifi.obs.client.wifi.model.Health;
-import com.wifi.obs.client.wifi.model.value.HealthQueryVO;
 import com.wifi.obs.client.wifi.support.log.WifiClientTrace;
 import feign.FeignException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -21,23 +20,17 @@ public class HealthClientQuery {
 	private final WifiHealthFeignClient wifiHealthFeignClient;
 
 	@WifiClientTrace
-	public Health query(WifiHealthRequestElement source) {
+	public HttpStatusResponse query(WifiHealthRequestElement source) {
 		try {
-			return Health.builder().statusInfo(getStatusInfo(source)).host(source.getHost()).build();
+			return HttpStatusResponse.of(execute(source));
 		} catch (URISyntaxException e) {
 			throw new WifiURISyntaxException(source.getUrl());
 		} catch (FeignException e) {
-			return Health.fail(source.getHost());
+			return HttpStatusResponse.of(HttpStatus.BAD_REQUEST);
 		}
 	}
 
-	private HealthQueryVO getStatusInfo(WifiHealthRequestElement source) throws URISyntaxException {
-		return HealthQueryVO.builder().info(executeQuery(source)).build();
-	}
-
-	private HttpStatusResponse executeQuery(WifiHealthRequestElement source)
-			throws URISyntaxException {
-		return HttpStatusResponse.of(
-				wifiHealthFeignClient.query(new URI(source.getUrl())).getStatusCode());
+	private HttpStatus execute(WifiHealthRequestElement source) throws URISyntaxException {
+		return wifiHealthFeignClient.query(new URI(source.getUrl())).getStatusCode();
 	}
 }
