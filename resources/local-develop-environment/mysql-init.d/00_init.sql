e2aa1fdf69b3d83b451901bbd8b7e8722a7c383b
belljun3395 <195850@jnu.ac.kr>
2024-02-13 12:34:59 +0900

    chore: 추가 기본 resource

---
+++ resources/local-develop-environment/mysql-init.d/00_init.sql
@@ -0,0 +1,12 @@
+CREATE
+    USER 'observer-local'@'localhost' IDENTIFIED BY 'observer-local';
+CREATE
+    USER 'observer-local'@'%' IDENTIFIED BY 'observer-local';
+
+GRANT ALL PRIVILEGES ON *.* TO
+    'observer-local'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO
+    'observer-local'@'%';
+
+CREATE
+    DATABASE api DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
