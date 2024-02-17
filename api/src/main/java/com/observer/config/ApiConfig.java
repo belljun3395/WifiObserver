98af418d763060a81c1e7915e0fafbfc9cfe3e77
belljun3395 <195850@jnu.ac.kr>
2024-02-17 20:45:28 +0900

    refactor: 수정 Bean Prefix 소문자로

--- api/src/main/java/com/observer/config/ApiConfig.java
+++ api/src/main/java/com/observer/config/ApiConfig.java
@@ -10,6 +10,6 @@ public class ApiConfig {
 	public static final String BASE_PACKAGE = "com.observer";
 	public static final String SERVICE_NAME = "observer";
 	public static final String MODULE_NAME = "api";
-	public static final String BEAN_NAME_PREFIX = "Api";
+	public static final String BEAN_NAME_PREFIX = "api";
 	public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
 }
