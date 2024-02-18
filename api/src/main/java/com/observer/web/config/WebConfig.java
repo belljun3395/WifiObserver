c907b982e482db87fb341136a7096133141fd0c1
belljun3395 <195850@jnu.ac.kr>
2024-02-19 08:10:49 +0900

    chore: 수정 bean prefix 소문자로

--- api/src/main/java/com/observer/web/config/WebConfig.java
+++ api/src/main/java/com/observer/web/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
 	public static final String BASE_PACKAGE = "com.observer.web";
 	public static final String SERVICE_NAME = "observer";
 	public static final String MODULE_NAME = "web";
-	public static final String BEAN_NAME_PREFIX = "Web";
+	public static final String BEAN_NAME_PREFIX = "web";
 	public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
 
 	@Bean
