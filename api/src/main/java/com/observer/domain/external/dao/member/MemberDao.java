af714c3bd827282732edafb4fbfa590f6f2b8983
belljun3395 <195850@jnu.ac.kr>
2024-03-05 19:01:39 +0900

    feat: 선언/구현 MemberDao/MemberDaoImpl

---
+++ api/src/main/java/com/observer/domain/external/dao/member/MemberDao.java
@@ -0,0 +1,20 @@
+package com.observer.domain.external.dao.member;
+
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import java.util.Optional;
+
+public interface MemberDao {
+
+	MemberEntity saveMember(MemberEntity memberEntity);
+
+	boolean existsByCertificationAndDeletedFalse(CertificationData certificationData);
+
+	Optional<MemberEntity> findByCertificationAndDeletedFalse(CertificationData certificationData);
+
+	boolean existsByApiKey(String apiKey);
+
+	Optional<MemberEntity> findByApiKeyAndDeletedFalse(String apiKey);
+
+	void delete(MemberEntity memberEntity);
+}
---
+++ api/src/main/java/com/observer/domain/external/dao/member/MemberDaoImpl.java
@@ -0,0 +1,48 @@
+package com.observer.domain.external.dao.member;
+
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import com.observer.persistence.member.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberDaoImpl implements MemberDao {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public MemberEntity saveMember(MemberEntity memberEntity) {
+		return memberRepository.save(memberEntity);
+	}
+
+	@Override
+	public boolean existsByCertificationAndDeletedFalse(CertificationData certificationData) {
+		return memberRepository.existsByCertificationAndDeletedFalse(certificationData);
+	}
+
+	@Override
+	public Optional<MemberEntity> findByCertificationAndDeletedFalse(
+			CertificationData certificationData) {
+		return memberRepository.findByCertificationAndDeletedFalse(certificationData);
+	}
+
+	@Override
+	public boolean existsByApiKey(String apiKey) {
+		return memberRepository.existsByApiKey(apiKey);
+	}
+
+	@Override
+	public Optional<MemberEntity> findByApiKeyAndDeletedFalse(String apiKey) {
+		return memberRepository.findByApiKeyAndDeletedFalse(apiKey);
+	}
+
+	@Override
+	public void delete(MemberEntity memberEntity) {
+		memberRepository.delete(memberEntity);
+	}
+}
