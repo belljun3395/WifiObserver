f7d0bad563b830d5616e84a8dc338f12ee73c301
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:09:00 +0900

    feat: 구현 DeviceEntity

---
+++ api/src/main/java/com/observer/entity/device/DeviceEntity.java
@@ -0,0 +1,39 @@
+package com.observer.entity.device;
+
+import com.observer.entity.RouterAndMemberFKBaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity(name = "device")
+@Table(name = "device_tb")
+@SQLDelete(sql = "UPDATE device_tb SET deleted=true WHERE id = ?")
+public class DeviceEntity extends RouterAndMemberFKBaseEntity {
+
+	@Builder.Default
+	@Enumerated(EnumType.STRING)
+	@Column(name = "device_type", nullable = false, updatable = false)
+	private DeviceType type = DeviceType.NOTEBOOK;
+
+	@Column(name = "mac", nullable = false, updatable = false)
+	private String mac;
+
+	@Builder.Default
+	@Column(name = "info", columnDefinition = "json")
+	private String info = "{}";
+}
