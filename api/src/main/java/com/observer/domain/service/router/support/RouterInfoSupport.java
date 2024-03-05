a703cf31050d7be951ad96d724ef4aa7842ea301
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:10:40 +0900

    feat: 구현 RouterInfoSupport

---
+++ api/src/main/java/com/observer/domain/service/router/support/RouterInfoSupport.java
@@ -0,0 +1,24 @@
+package com.observer.domain.service.router.support;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class RouterInfoSupport {
+
+	private Long memberId;
+	private Long routerId;
+	private String serviceType;
+	@Builder.Default private Long cycle = 10L;
+	@Builder.Default private Long standardTime = 9L;
+	private String host;
+}
