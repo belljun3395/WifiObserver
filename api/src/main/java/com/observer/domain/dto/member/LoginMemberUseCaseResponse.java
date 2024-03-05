1d1b3b70420692412d98f1223dda6c6833747e3c
belljun3395 <195850@jnu.ac.kr>
2024-03-05 19:02:47 +0900

    feat: 구현 LoginMemberUseCase

---
+++ api/src/main/java/com/observer/domain/dto/member/LoginMemberUseCaseResponse.java
@@ -0,0 +1,19 @@
+package com.observer.domain.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class LoginMemberUseCaseResponse {
+
+	private String apiKey;
+}
---
+++ api/src/main/java/com/observer/domain/usecase/member/LoginMemberUseCase.java
@@ -0,0 +1,51 @@
+package com.observer.domain.usecase.member;
+
+import com.observer.domain.dto.member.LoginMemberUseCaseRequest;
+import com.observer.domain.dto.member.LoginMemberUseCaseResponse;
+import com.observer.domain.external.dao.member.MemberDao;
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import com.observer.entity.member.MemberPasswordData;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoginMemberUseCase {
+
+	private final MemberDao memberDao;
+
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public LoginMemberUseCaseResponse execute(LoginMemberUseCaseRequest request) {
+		final CertificationData certification =
+				CertificationData.builder().certification(request.getCertification()).build();
+		final MemberPasswordData password =
+				MemberPasswordData.builder().password(request.getPassword()).build();
+
+		Optional<MemberEntity> memberEntitySource =
+				memberDao.findByCertificationAndDeletedFalse(certification);
+		if (memberEntitySource.isEmpty()) {
+			throw new IllegalStateException(
+					"Member is not found. certification: " + certification.getCertification());
+		}
+		MemberEntity memberEntity = memberEntitySource.get();
+
+		MemberPasswordData entityPassword = memberEntity.getPassword();
+		boolean isMatchPassword =
+				passwordEncoder.matches(password.getPassword(), entityPassword.getPassword());
+		if (!isMatchPassword) {
+			throw new IllegalArgumentException("Password is not matched.");
+		}
+
+		String apiKey = memberEntity.getApiKey();
+
+		return LoginMemberUseCaseResponse.builder().apiKey(apiKey).build();
+	}
+}
