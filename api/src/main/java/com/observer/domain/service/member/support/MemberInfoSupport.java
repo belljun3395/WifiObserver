e4d08fb200ae480173d8e66b57fc6386430afe3d
belljun3395 <195850@jnu.ac.kr>
2024-03-05 22:15:17 +0900

    feat: 구현 MemberInfoSupport

---
+++ api/src/main/java/com/observer/domain/service/member/support/MemberInfoSupport.java
@@ -0,0 +1,22 @@
+package com.observer.domain.service.member.support;
+
+import com.observer.entity.member.MemberStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class MemberInfoSupport {
+
+	private Long memberId;
+	private MemberStatus status;
+	private String resource;
+}
