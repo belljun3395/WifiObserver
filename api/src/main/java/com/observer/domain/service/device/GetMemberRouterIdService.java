4a3c2dd44cd230b241810cb71ed4b60c9443547f
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:10:25 +0900

    feat: 구현 GetMemberRouterIdService

---
+++ api/src/main/java/com/observer/domain/service/device/GetMemberRouterIdService.java
@@ -0,0 +1,25 @@
+package com.observer.domain.service.device;
+
+import com.observer.domain.service.router.support.GetRouterInfoSupportService;
+import com.observer.domain.service.router.support.RouterInfoSupport;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GetMemberRouterIdService {
+
+	private final GetRouterInfoSupportService getRouterInfoSupportService;
+
+	public Optional<Long> execute(Long routerId, Long memberId) {
+		RouterInfoSupport routerInfoSupport =
+				getRouterInfoSupportService.execute(routerId).orElseThrow(IllegalStateException::new);
+		if (!routerInfoSupport.getMemberId().equals(memberId)) {
+			return Optional.empty();
+		}
+		return Optional.of(routerInfoSupport.getRouterId());
+	}
+}
