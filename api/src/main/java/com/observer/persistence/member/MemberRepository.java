cdfa6b2865fb0ea6d09901e3ac1e3743fe7ee303
belljun3395 <195850@jnu.ac.kr>
2024-03-05 19:00:34 +0900

    feat: 선언 MemberRepository

---
+++ api/src/main/java/com/observer/persistence/member/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.observer.persistence.member;
+
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+	boolean existsByCertificationAndDeletedFalse(CertificationData certification);
+
+	Optional<MemberEntity> findByCertificationAndDeletedFalse(CertificationData certification);
+
+	boolean existsByApiKey(String apiKey);
+
+	Optional<MemberEntity> findByApiKeyAndDeletedFalse(String apiKey);
+}
