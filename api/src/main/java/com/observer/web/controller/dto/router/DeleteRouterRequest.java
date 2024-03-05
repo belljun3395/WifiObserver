068afd6fa8766b9917a0db39de6961783d0080ef
belljun3395 <195850@jnu.ac.kr>
2024-03-05 22:17:33 +0900

    feat: 구현 Router Api 요청 객체

---
+++ api/src/main/java/com/observer/web/controller/dto/router/DeleteRouterRequest.java
@@ -0,0 +1,19 @@
+package com.observer.web.controller.dto.router;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DeleteRouterRequest {
+
+	private Long routerId;
+}
