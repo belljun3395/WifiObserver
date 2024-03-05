541dae134883169b0eb132a69f0dbcf96aa60358
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:09:59 +0900

    feat: 선언/구현 DeviceDao/DeviceDaoImpl

---
+++ api/src/main/java/com/observer/domain/external/dao/device/DeviceDao.java
@@ -0,0 +1,15 @@
+package com.observer.domain.external.dao.device;
+
+import com.observer.entity.device.DeviceEntity;
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceDao {
+	Optional<DeviceEntity> findByIdAndDeletedFalse(Long id);
+
+	List<DeviceEntity> findAllByRouterIdAndDeletedFalse(Long routerServiceId);
+
+	DeviceEntity saveDevice(DeviceEntity deviceEntity);
+
+	void deleteDevice(DeviceEntity deviceEntity);
+}
---
+++ api/src/main/java/com/observer/domain/external/dao/device/DeviceDaoImpl.java
@@ -0,0 +1,37 @@
+package com.observer.domain.external.dao.device;
+
+import com.observer.entity.device.DeviceEntity;
+import com.observer.persistence.device.DeviceRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class DeviceDaoImpl implements DeviceDao {
+
+	private final DeviceRepository deviceRepository;
+
+	@Override
+	public Optional<DeviceEntity> findByIdAndDeletedFalse(Long id) {
+		return deviceRepository.findByIdAndDeletedFalse(id);
+	}
+
+	@Override
+	public List<DeviceEntity> findAllByRouterIdAndDeletedFalse(Long routerServiceId) {
+		return deviceRepository.findAllByRouterIdAndDeletedFalse(routerServiceId);
+	}
+
+	@Override
+	public DeviceEntity saveDevice(DeviceEntity deviceEntity) {
+		return deviceRepository.save(deviceEntity);
+	}
+
+	@Override
+	public void deleteDevice(DeviceEntity deviceEntity) {
+		deviceRepository.delete(deviceEntity);
+	}
+}
