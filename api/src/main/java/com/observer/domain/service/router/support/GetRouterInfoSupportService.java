5ebf42658e465c137edfeaa571ce283bca8c06cb
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:10:35 +0900

    feat: 구현 GetRouterInfoSupportService

---
+++ api/src/main/java/com/observer/domain/service/router/support/GetRouterInfoSupportService.java
@@ -0,0 +1,35 @@
+package com.observer.domain.service.router.support;
+
+import com.observer.domain.external.dao.router.RouterDao;
+import com.observer.entity.router.RouterEntity;
+import com.observer.entity.router.RouterStatus;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GetRouterInfoSupportService {
+
+	private final RouterDao routerDao;
+
+	public Optional<RouterInfoSupport> execute(Long routerId) {
+		Optional<RouterEntity> routerEntitySource =
+				routerDao.findByIdAndStatusAndDeletedFalse(routerId, RouterStatus.ON);
+		if (routerEntitySource.isEmpty()) {
+			return Optional.empty();
+		}
+		RouterEntity routerEntity = routerEntitySource.get();
+		return Optional.of(
+				RouterInfoSupport.builder()
+						.memberId(routerEntity.getMemberId())
+						.routerId(routerEntity.getId())
+						.serviceType(routerEntity.getServiceType().getType())
+						.cycle(routerEntity.getCycle())
+						.standardTime(routerEntity.getStandardTime())
+						.host(routerEntity.getHost())
+						.build());
+	}
+}
