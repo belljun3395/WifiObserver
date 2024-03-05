787b8c71a74043731a2b8b422ece1705a281665f
belljun3395 <195850@jnu.ac.kr>
2024-03-06 00:13:12 +0900

    feat: 구현 Device Api 요청 객체

---
+++ api/src/main/java/com/observer/web/controller/dto/device/DeleteDeviceRequest.java
@@ -0,0 +1,20 @@
+package com.observer.web.controller.dto.device;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DeleteDeviceRequest {
+
+	private Long routeId;
+	private Long deviceId;
+}
