34528b8b741413c41a4d00ff23527d7264f96ed4
belljun3395 <195850@jnu.ac.kr>
2024-03-05 19:03:01 +0900

    feat: 구현 RenewalApiKeyUseCase

---
+++ api/src/main/java/com/observer/domain/usecase/member/RenewalApiKeyUseCase.java
@@ -0,0 +1,58 @@
+package com.observer.domain.usecase.member;
+
+import com.observer.domain.dto.member.RenewalApiKeyUseCaseRequest;
+import com.observer.domain.dto.member.RenewalApiKeyUseCaseResponse;
+import com.observer.domain.external.dao.member.MemberDao;
+import com.observer.domain.service.member.ApiKeyGenerateService;
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import com.observer.entity.member.MemberPasswordData;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RenewalApiKeyUseCase {
+
+	private final MemberDao memberDao;
+
+	private final ApiKeyGenerateService apiKeyGenerateService;
+
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public RenewalApiKeyUseCaseResponse execute(RenewalApiKeyUseCaseRequest request) {
+		final CertificationData certification =
+				CertificationData.builder().certification(request.getCertification()).build();
+		MemberPasswordData password =
+				MemberPasswordData.builder().password(request.getPassword()).build();
+
+		Optional<MemberEntity> memberEntitySource =
+				memberDao.findByCertificationAndDeletedFalse(certification);
+		if (memberEntitySource.isEmpty()) {
+			throw new IllegalStateException(
+					"Member is not found. certification: " + certification.getCertification());
+		}
+
+		MemberEntity memberEntity = memberEntitySource.get();
+
+		MemberPasswordData entityPassword = memberEntity.getPassword();
+		boolean isMatchPassword =
+				passwordEncoder.matches(password.getPassword(), entityPassword.getPassword());
+		if (isMatchPassword) {
+			throw new IllegalArgumentException("Password is not matched.");
+		}
+
+		String apiKey = apiKeyGenerateService.generate();
+
+		memberEntity = memberEntity.toBuilder().apiKey(apiKey).build();
+		memberDao.saveMember(memberEntity);
+
+		return RenewalApiKeyUseCaseResponse.builder().apiKey(apiKey).build();
+	}
+}
