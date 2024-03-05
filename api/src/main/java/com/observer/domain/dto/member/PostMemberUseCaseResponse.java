487d369632b76bd94e9b021c71ea2c689d055068
belljun3395 <195850@jnu.ac.kr>
2024-03-05 19:02:54 +0900

    feat: 구현 PostMemberUseCase

---
+++ api/src/main/java/com/observer/domain/dto/member/PostMemberUseCaseResponse.java
@@ -0,0 +1,22 @@
+package com.observer.domain.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class PostMemberUseCaseResponse {
+
+	private Long memberId;
+	private String certification;
+	private String password;
+	private String apiKey;
+}
---
+++ api/src/main/java/com/observer/domain/usecase/member/PostMemberUseCase.java
@@ -0,0 +1,67 @@
+package com.observer.domain.usecase.member;
+
+import com.observer.domain.dto.member.PostMemberUseCaseRequest;
+import com.observer.domain.dto.member.PostMemberUseCaseResponse;
+import com.observer.domain.external.dao.member.MemberDao;
+import com.observer.domain.service.member.ApiKeyGenerateService;
+import com.observer.entity.member.CertificationData;
+import com.observer.entity.member.MemberEntity;
+import com.observer.entity.member.MemberPasswordData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostMemberUseCase {
+
+	private final MemberDao memberDao;
+
+	private final ApiKeyGenerateService apiKeyGenerateService;
+
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public PostMemberUseCaseResponse execute(PostMemberUseCaseRequest request) {
+		final CertificationData certification =
+				CertificationData.builder().certification(request.getCertification()).build();
+		MemberPasswordData password =
+				MemberPasswordData.builder().password(request.getPassword()).build();
+
+		log.debug("Check duplicate certification. certification: {}", certification.getCertification());
+		boolean isDuplicateId = memberDao.existsByCertificationAndDeletedFalse(certification);
+		if (isDuplicateId) {
+			throw new IllegalStateException(
+					"Certification is duplicated. certification: " + certification.getCertification());
+		}
+
+		password = encodePassword(password);
+
+		String apiKey = apiKeyGenerateService.generate();
+
+		// todo certification을 기준으로 락을 걸어 처리 해야 함
+		MemberEntity memberSource =
+				MemberEntity.builder()
+						.certification(certification)
+						.password(password)
+						.apiKey(apiKey)
+						.build();
+		MemberEntity memberEntity = memberDao.saveMember(memberSource);
+		log.debug("Save member. memberSource: {}", memberSource);
+
+		return PostMemberUseCaseResponse.builder()
+				.memberId(memberEntity.getId())
+				.certification(memberEntity.getCertification().getCertification())
+				.password(memberEntity.getPassword().getPassword())
+				.apiKey(memberEntity.getApiKey())
+				.build();
+	}
+
+	private MemberPasswordData encodePassword(MemberPasswordData password) {
+		String encodedPassword = passwordEncoder.encode(password.getPassword());
+		return password.toBuilder().password(encodedPassword).build();
+	}
+}
