0a22c7b2b4e138a3b6db3528be8fdfe893102e49
belljun3395 <195850@jnu.ac.kr>
2024-03-08 23:27:48 +0900

    feat: 구현 ConnectRecordDataConverter

---
+++ api/src/main/java/com/observer/entity/history/ConnectRecordDataConverter.java
@@ -0,0 +1,34 @@
+package com.observer.entity.history;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Converter
+@Component
+@RequiredArgsConstructor
+public class ConnectRecordDataConverter implements AttributeConverter<ConnectRecordData, String> {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public String convertToDatabaseColumn(ConnectRecordData attribute) {
+		try {
+			return objectMapper.writeValueAsString(attribute);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public ConnectRecordData convertToEntityAttribute(String dbData) {
+		try {
+			return objectMapper.readValue(dbData, ConnectRecordData.class);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
