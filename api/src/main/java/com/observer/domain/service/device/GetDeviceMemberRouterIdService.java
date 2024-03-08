6bc7b2844890e58e943b55513d769665ec25541e
belljun3395 <195850@jnu.ac.kr>
2024-03-08 23:25:42 +0900

    refactor: 추가 클래스명에 Member prefix

--- api/src/main/java/com/observer/domain/service/device/GetDeviceMemberRouterIdService.java
+++ api/src/main/java/com/observer/domain/service/device/GetDeviceMemberRouterIdService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class GetMemberRouterIdService {
+public class GetDeviceMemberRouterIdService {
 
 	private final GetRouterInfoSupportService getRouterInfoSupportService;
 
