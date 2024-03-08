098618328bc4d4787646b92749d7b1a97ca9d1de
belljun3395 <195850@jnu.ac.kr>
2024-03-08 23:27:41 +0900

    feat: 구현 ConnectRecordData

---
+++ api/src/main/java/com/observer/entity/history/ConnectRecordData.java
@@ -0,0 +1,20 @@
+package com.observer.entity.history;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ConnectRecordData {
+
+	private Long monthlyRecord;
+	private Long dailyRecord;
+}
