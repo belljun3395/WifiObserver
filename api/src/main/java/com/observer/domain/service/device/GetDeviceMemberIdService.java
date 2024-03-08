5c340d35238cf7ba6ad6f2e67d605e0b541e9f06
belljun3395 <195850@jnu.ac.kr>
2024-03-08 23:33:46 +0900

    feat: 구현 GetDeviceMemberIdService

---
+++ api/src/main/java/com/observer/domain/service/device/GetDeviceMemberIdService.java
@@ -0,0 +1,21 @@
+package com.observer.domain.service.device;
+
+import com.observer.domain.service.member.support.GetMemberInfoSupportService;
+import com.observer.domain.service.member.support.MemberInfoSupport;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GetDeviceMemberIdService {
+
+	private final GetMemberInfoSupportService getMemberInfoSupportService;
+
+	public Long execute(String apiKey) {
+		MemberInfoSupport memberInfoSupport =
+				getMemberInfoSupportService.execute(apiKey).orElseThrow(IllegalStateException::new);
+		return memberInfoSupport.getMemberId();
+	}
+}
