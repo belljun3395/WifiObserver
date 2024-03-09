ee0a3942c1ed08b04a009791aa7062914fb66d1d
belljun3395 <195850@jnu.ac.kr>
2024-03-09 21:48:15 +0900

    feat: 구현 BatchLaunchConfig

---
+++ batch/src/main/java/com/observer/batch/config/BatchLaunchConfig.java
@@ -0,0 +1,26 @@
+package com.observer.batch.config;
+
+import static com.observer.batch.config.BatchConfig.BEAN_NAME_PREFIX;
+import static com.observer.batch.config.DelegatedBatchConfigurer.DELEGATED_BATCH_CONFIGURER_BEAN_NAME;
+
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchLaunchConfig {
+
+	public static final String JOB_LAUNCHER_BEAN_NAME = BEAN_NAME_PREFIX + "JobLauncher";
+
+	@Bean(name = JOB_LAUNCHER_BEAN_NAME)
+	public JobLauncher jobLauncher(
+			@Qualifier(value = DELEGATED_BATCH_CONFIGURER_BEAN_NAME) BatchConfigurer configurer)
+			throws Exception {
+		SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+		simpleJobLauncher.setJobRepository(configurer.getJobRepository());
+		return simpleJobLauncher;
+	}
+}
