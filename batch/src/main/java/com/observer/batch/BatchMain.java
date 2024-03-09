c2557e7c860e0103d0b681afc625ce850fa5fede
belljun3395 <195850@jnu.ac.kr>
2024-03-09 21:47:16 +0900

    feat: 구현 BatchMain

---
+++ batch/src/main/java/com/observer/batch/BatchMain.java
@@ -0,0 +1,13 @@
+package com.observer.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchMain.class, args);
+    }
+}
+
