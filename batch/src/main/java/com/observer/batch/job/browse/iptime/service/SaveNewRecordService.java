674e55106465930df85bcb9f7269cbd4a2f66ea7
belljun3395 <195850@jnu.ac.kr>
2024-03-16 23:44:56 +0900

    fix: 해결 해지 기록이 있는 경우를 고려하지 못한 문제

--- batch/src/main/java/com/observer/batch/job/browse/iptime/service/SaveNewRecordService.java
+++ batch/src/main/java/com/observer/batch/job/browse/iptime/service/SaveNewRecordService.java
@@ -1,12 +1,18 @@
 package com.observer.batch.job.browse.iptime.service;
 
+import com.observer.data.config.JpaDataSourceConfig;
 import com.observer.data.entity.history.ConnectHistoryEntity;
 import com.observer.data.entity.history.ConnectStatus;
 import com.observer.data.persistence.history.connect.ConnectHistoryRepository;
+import com.observer.data.support.RecordMapper;
+import com.observer.data.support.RecordParser;
+import com.observer.data.support.RecordSupportInfo;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
@@ -15,7 +21,41 @@ public class SaveNewRecordService {
 
 	private final ConnectHistoryRepository connectHistoryRepository;
 
+	private final RecordParser recordParser;
+	private final RecordMapper recordMapper;
+
+	@Transactional(transactionManager = JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	public ConnectHistoryEntity execute(Long newConnectDeviceId, Long routerId, LocalDateTime now) {
+		Optional<ConnectHistoryEntity> disconnectHistoryEntitySource =
+				connectHistoryRepository.findByDeviceIdAndConnectStatusAndDeletedFalse(
+						newConnectDeviceId, ConnectStatus.DISCONNECTED);
+		// 이전 연결 기록 및 해지 기록이 없는 경우: 신규 기록 생성
+		if (disconnectHistoryEntitySource.isEmpty()) {
+			RecordSupportInfo recordInfo = RecordSupportInfo.builder().build();
+			return connectHistoryRepository.save(
+					ConnectHistoryEntity.builder()
+							.deviceId(newConnectDeviceId)
+							.routerId(routerId)
+							.connectTime(now)
+							.checkTime(now)
+							.disConnectTime(now)
+							.record(recordMapper.execute(recordInfo))
+							.connectStatus(ConnectStatus.CONNECTED)
+							.build());
+		}
+		// 해지 기록이 있는 경우: 재접속 기록 생성
+		ConnectHistoryEntity disConnectHistoryEntity = disconnectHistoryEntitySource.get();
+		LocalDateTime disConnectTime = disConnectHistoryEntity.getDisConnectTime();
+		String record = disConnectHistoryEntity.getRecord();
+		RecordSupportInfo recordInfo = recordParser.execute(record);
+
+		if (disConnectTime.getMonth() != now.getMonth()) {
+			recordInfo.resetMonth();
+		}
+		if (disConnectTime.getDayOfWeek() != now.getDayOfWeek()) {
+			recordInfo.resetDay();
+		}
+
 		return connectHistoryRepository.save(
 				ConnectHistoryEntity.builder()
 						.deviceId(newConnectDeviceId)
@@ -23,6 +63,7 @@ public class SaveNewRecordService {
 						.connectTime(now)
 						.checkTime(now)
 						.disConnectTime(now)
+						.record(recordMapper.execute(recordInfo))
 						.connectStatus(ConnectStatus.CONNECTED)
 						.build());
 	}
