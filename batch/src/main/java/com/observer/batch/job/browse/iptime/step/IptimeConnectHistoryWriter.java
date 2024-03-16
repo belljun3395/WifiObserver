fa7950423f8c9123f4aea380669ada798abbd771
belljun3395 <195850@jnu.ac.kr>
2024-03-16 19:44:46 +0900

    refactor: 반영 record 관련 로직 service로 분리

--- batch/src/main/java/com/observer/batch/job/browse/iptime/step/IptimeConnectHistoryWriter.java
+++ batch/src/main/java/com/observer/batch/job/browse/iptime/step/IptimeConnectHistoryWriter.java
@@ -1,6 +1,8 @@
 package com.observer.batch.job.browse.iptime.step;
 
-import com.observer.batch.job.utils.ZeroSecondDateTimeCalculator;
+import com.observer.batch.job.browse.iptime.service.CheckRecordService;
+import com.observer.batch.job.browse.iptime.service.SaveDisconnectRecordService;
+import com.observer.batch.job.browse.iptime.service.SaveNewRecordService;
 import com.observer.client.router.support.dto.response.RouterUser;
 import com.observer.client.router.support.dto.response.RouterUsersResponse;
 import com.observer.data.config.JpaDataSourceConfig;
@@ -11,9 +13,6 @@ import com.observer.data.entity.router.RouterEntity;
 import com.observer.data.persistence.device.DeviceRepository;
 import com.observer.data.persistence.history.connect.ConnectHistoryRepository;
 import com.observer.data.persistence.router.RouterRepository;
-import com.observer.data.support.RecordMapper;
-import com.observer.data.support.RecordParser;
-import com.observer.data.support.RecordSupportInfo;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
@@ -35,10 +34,12 @@ public class IptimeConnectHistoryWriter implements ItemWriter<RouterUsersRespons
 
 	private final RouterRepository routerRepository;
 	private final DeviceRepository deviceRepository;
+
 	private final ConnectHistoryRepository connectHistoryRepository;
 
-	private final RecordParser recordParser;
-	private final RecordMapper recordMapper;
+	private final SaveDisconnectRecordService saveDisconnectRecordService;
+	private final CheckRecordService checkRecordService;
+	private final SaveNewRecordService saveNewRecordService;
 
 	@Override
 	@Transactional(transactionManager = JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
@@ -80,13 +81,11 @@ public class IptimeConnectHistoryWriter implements ItemWriter<RouterUsersRespons
 				// 접속 기록(historyEntity)의 Device Id가 접속 중인 Id(connectDeviceIds) 목록에 없는 경우: 접속이 끊어진 경우
 				final Long historyEntityDeviceId = historyEntity.getDeviceId();
 				if (!ifContainsRemoveDeviceIds.contains(historyEntityDeviceId)) {
-					log.info("======> saveDisconnectRecord() deviceId: {}", historyEntity.getDeviceId());
-					saveDisconnectRecord(historyEntity, now);
+					saveDisconnectRecordService.execute(historyEntity, now);
 					continue;
 				}
 				// 접속 기록(historyEntity)의 Device Id가 접속 중인 Id(connectDeviceIds) 목록에 있는 경우: 접속이 유지 중인 경우
-				log.info("======> checkRecord() deviceId: {}", historyEntity.getDeviceId());
-				checkRecord(historyEntity, now);
+				checkRecordService.execute(historyEntity, now);
 				ifContainsRemoveDeviceIds.remove(historyEntityDeviceId);
 			}
 
@@ -94,8 +93,7 @@ public class IptimeConnectHistoryWriter implements ItemWriter<RouterUsersRespons
 			Set<Long> newConnectDeviceIds = ifContainsRemoveDeviceIds;
 			log.info("======> newConnectDeviceIds: {}", newConnectDeviceIds);
 			for (Long newConnectDeviceId : newConnectDeviceIds) {
-				log.info("======> saveNewRecord() deviceId: {}", newConnectDeviceId);
-				saveNewRecord(newConnectDeviceId, routerId, now);
+				saveNewRecordService.execute(newConnectDeviceId, routerId, now);
 			}
 		}
 		log.info("======================================================");
@@ -120,109 +118,5 @@ public class IptimeConnectHistoryWriter implements ItemWriter<RouterUsersRespons
 				.filter(s -> Objects.nonNull(deviceMacAndIdInfos.get(s)))
 				.map(deviceMacAndIdInfos::get)
 				.collect(Collectors.toSet());
-	}
-
-	private void saveDisconnectRecord(ConnectHistoryEntity historyEntity, LocalDateTime now) {
-		final LocalDateTime connectDateTime = historyEntity.getCheckTime();
-		final long connectedMinutes =
-				ZeroSecondDateTimeCalculator.between(connectDateTime, now).toMinutes();
-		final String record = historyEntity.getRecord();
-
-		RecordSupportInfo recordSupportInfo = recordParser.execute(record);
-		String newRecord = getNewRecord(now, connectDateTime, recordSupportInfo, connectedMinutes);
-
-		connectHistoryRepository.save(
-				historyEntity.toBuilder()
-						.disConnectTime(now)
-						.connectStatus(ConnectStatus.DISCONNECTED)
-						.record(newRecord)
-						.build());
-	}
-
-	private void checkRecord(ConnectHistoryEntity historyEntity, LocalDateTime now) {
-		final LocalDateTime connectDateTime = historyEntity.getCheckTime();
-		final long connectedMinutes =
-				ZeroSecondDateTimeCalculator.between(connectDateTime, now).toMinutes();
-		final String record = historyEntity.getRecord();
-
-		boolean isNewEntity = false;
-		RecordSupportInfo recordSupportInfo = recordParser.execute(record);
-
-		// 접속한 시간과 접속이 끊긴 시간 사이에 월이 바뀐 경우: 월 단위로 누적 시간 계산
-		if (connectDateTime.getMonth() != now.getMonth()) {
-			// 이전 월까지의 누적 시간 계산
-			final LocalDateTime nowMonthStartDateTime =
-					LocalDateTime.of(now.getYear(), now.getMonth(), 1, 0, 0);
-			long beforeNowAccumulateMinutes =
-					ZeroSecondDateTimeCalculator.between(connectDateTime, nowMonthStartDateTime).toMinutes();
-			recordSupportInfo.accumulate(beforeNowAccumulateMinutes);
-			String beforeNowRecord = recordMapper.execute(recordSupportInfo);
-			recordSupportInfo.resetMonth();
-
-			// 이전 월까지의 누적 시간을 기록
-			connectHistoryRepository.save(
-					historyEntity.toBuilder()
-							.disConnectTime(nowMonthStartDateTime)
-							.connectStatus(ConnectStatus.DISCONNECTED)
-							.record(beforeNowRecord)
-							.build());
-
-			isNewEntity = true;
-			long remainAccumulateMinutes = connectedMinutes - beforeNowAccumulateMinutes;
-			recordSupportInfo.accumulate(remainAccumulateMinutes);
-		}
-
-		String newRecord = getNewRecord(now, connectDateTime, recordSupportInfo, connectedMinutes);
-
-		if (isNewEntity) {
-			connectHistoryRepository.save(
-					ConnectHistoryEntity.builder()
-							.deviceId(historyEntity.getDeviceId())
-							.routerId(historyEntity.getRouterId())
-							.connectTime(now)
-							.checkTime(now)
-							.connectStatus(ConnectStatus.CONNECTED)
-							.record(newRecord)
-							.build());
-		} else {
-			connectHistoryRepository.save(
-					historyEntity.toBuilder()
-							.checkTime(now)
-							.connectStatus(ConnectStatus.CONNECTED)
-							.record(newRecord)
-							.build());
-		}
-	}
-
-	private String getNewRecord(
-			LocalDateTime now,
-			LocalDateTime connectDateTime,
-			RecordSupportInfo recordSupportInfo,
-			long connectedMinutes) {
-		if (connectDateTime.getDayOfWeek() != now.getDayOfWeek()) {
-			final LocalDateTime nowStartDateTime =
-					LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 0, 0);
-			long beforeNowAccumulateMinutes =
-					ZeroSecondDateTimeCalculator.between(connectDateTime, nowStartDateTime).toMinutes();
-			recordSupportInfo.accumulate(beforeNowAccumulateMinutes);
-			recordSupportInfo.resetDay();
-			long remainAccumulateMinutes = connectedMinutes - beforeNowAccumulateMinutes;
-			recordSupportInfo.accumulate(remainAccumulateMinutes);
-		} else {
-			recordSupportInfo.accumulate(connectedMinutes);
-		}
-		return recordMapper.execute(recordSupportInfo);
-	}
-
-	private void saveNewRecord(Long newConnectDeviceId, Long routerId, LocalDateTime now) {
-		connectHistoryRepository.save(
-				ConnectHistoryEntity.builder()
-						.deviceId(newConnectDeviceId)
-						.routerId(routerId)
-						.connectTime(now)
-						.checkTime(now)
-						.disConnectTime(now)
-						.connectStatus(ConnectStatus.CONNECTED)
-						.build());
 	}
 }
