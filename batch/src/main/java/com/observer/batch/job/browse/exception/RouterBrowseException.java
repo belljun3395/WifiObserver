72890035c5726bfa66a009bdb78412e2a62b3b3b
belljun3395 <195850@jnu.ac.kr>
2024-03-16 15:20:17 +0900

    feat: 구현 RouterBrowseException

---
+++ batch/src/main/java/com/observer/batch/job/browse/exception/RouterBrowseException.java
@@ -0,0 +1,8 @@
+package com.observer.batch.job.browse.exception;
+
+public class RouterBrowseException extends AbstractRouterException {
+
+	public RouterBrowseException(String request) {
+		super(request);
+	}
+}
